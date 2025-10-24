7789d2ce4391af884504cb968ab28a97b3187970
manvredo <manvredo@gmail.com>
2025-10-24 11:23:23 +0200

    .

--- next.config.js
+++ next.config.js
@@ -8,6 +8,16 @@ const nextConfig = {
       },
     ],
   },
+  // Favicon nur von Cloudinary
+  async redirects() {
+    return [
+      {
+        source: '/favicon.ico',
+        destination: 'https://res.cloudinary.com/dhjcx2xdd/image/upload/v1760947127/hi1xus7wdvma3mlv74ii.png',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
