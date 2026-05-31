aabe350b46db0ed62e17305a3ad33f8d6c2a37f4
manvredo <manvredo@gmail.com>
2026-05-31 20:56:45 +0200

    Fix vertical gap in shop page grid

--- app/shop/page.js
+++ app/shop/page.js
@@ -88,7 +88,7 @@ export default function ShopPage() {
       </div>
 
       {/* Products Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-[228px] gap-x-12">
         {filteredProducts.map(product => (
           <ProductCard
             key={product.id}
