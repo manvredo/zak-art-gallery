f076a1770d4305cf8761986a24f52d91c0890942
manvredo <manvredo@gmail.com>
2026-06-01 09:10:35 +0200

    Shop: set vertical gap to ~3cm (113px)

--- app/shop/page.js
+++ app/shop/page.js
@@ -88,7 +88,7 @@ export default function ShopPage() {
       </div>
 
       {/* Products Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-[130px] gap-x-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-[113px] gap-x-8">
         {filteredProducts.map((product, index) => (
           <ProductCard
             key={product.id}
